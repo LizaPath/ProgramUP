98cb6f9d638ca39f4269cc9e95d0e1618c185018
Patrusheva <copecat414@mail.ru>
2026-06-23 22:38:24 +0300

    Конфликт разрешен вручную

--- module1.cs
+++ module1.cs
@@ -1,1 +1,16 @@
 // module1.cs - консольное приложение
+using System;
+
+namespace SportSectionManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SportSection Manager v0.1");
+            Console.WriteLine("Система учёта студентов спортивной секции");
+            Console.WriteLine("Разработчик: [Патрушева]");
+            Console.ReadKey();
+        }
+    }
+}
